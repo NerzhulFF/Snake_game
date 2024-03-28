264573c62645b16bd11f30a2db5a6b7fb4e0a571
Nikita <124782857+NerzhulFF@users.noreply.github.com>
2024-03-28 11:08:25 +0300

    Delete Food.js

--- Food.js
+++
@@ -1,7 +0,0 @@
-import Snake from "./Snake.js";
-
-export default class Food extends Snake {
-  constructor(startX, startY, fieldElement, CELLS_QUANTITY) {
-    super(startX, startY, fieldElement, CELLS_QUANTITY);
-  }
-}
